15fd455ed00c78c5b7ed0fc3273ff15488bd75e5
Brayan Esteves <brayan.esteves93@gmail.com>
2023-05-19 19:04:39 -0400

    Refresh Token + Collections Postman (Update) [19.05.2023]

--- serviceweb-apirest-nodejs/routes/authRoute.js
+++ serviceweb-apirest-nodejs/routes/authRoute.js
@@ -1,16 +1,17 @@
 const express            = require("express");
-const { createUser, createUserSingle, loginUserCtrl, getUsers, getUser, updatedUser, deleteUser, blockUser, unblockUser } = require("../controllers/userCtrl");
+const { createUser, createUserSingle, loginUserCtrl, getUsers, getUser, updatedUser, deleteUser, blockUser, unblockUser, handleRefreshToken } = require("../controllers/userCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router             = express.Router();
 
-router.post  ( '/register-single', createUserSingle);
-router.post  (        '/register', createUser);
-router.post  (           '/login', loginUserCtrl);
-router.get   (       '/all-users', authMiddleware, isAdmin, getUsers);
-router.get   (    '/get-user/:id', authMiddleware, isAdmin, getUser);
-router.delete( '/delete-user/:id', authMiddleware, isAdmin, deleteUser);
-router.put   (  '/block-user/:id', authMiddleware, isAdmin, blockUser);
-router.put   ('/unblock-user/:id', authMiddleware, isAdmin, unblockUser);
-router.put   ( '/update-user/:id', authMiddleware, updatedUser);
+router.post  (   '/register-single', createUserSingle);
+router.post  (          '/register', createUser);
+router.post  (             '/login', loginUserCtrl);
+router.get   (         '/all-users', authMiddleware, isAdmin, getUsers);
+router.get   (      '/get-user/:id', authMiddleware, isAdmin, getUser);
+router.delete(   '/delete-user/:id', authMiddleware, isAdmin, deleteUser);
+router.put   (    '/block-user/:id', authMiddleware, isAdmin, blockUser);
+router.put   (  '/unblock-user/:id', authMiddleware, isAdmin, unblockUser);
+router.put   (   '/update-user/:id', authMiddleware, updatedUser);
+router.get   ('/refresh-token-user', handleRefreshToken);
 
 module.exports = router;
