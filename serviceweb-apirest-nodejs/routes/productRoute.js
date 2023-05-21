10d59bb622aa5e6cfefc269756ac1c6da1d1bf40
Brayan Esteves <brayan.esteves93@gmail.com>
2023-05-20 22:06:35 -0400

    Fix Products  [20.05.2023]

--- serviceweb-apirest-nodejs/routes/productRoute.js
+++ serviceweb-apirest-nodejs/routes/productRoute.js
@@ -3,10 +3,10 @@ const { createProduct, getProduct, getAllProduct, updateProduct, deleteProduct }
 const { isAdmin, authMiddleware } = require('../middlewares/authMiddleware');
 const router  = express.Router();
 
-router.post  (       "/add-product", isAdmin, authMiddleware, createProduct);
+router.post  (       "/add-product", authMiddleware, isAdmin, createProduct);
 router.get   (  "/find-product/:id", getProduct);
 router.get   (       "/all-product", getAllProduct);
-router.put   ("/update-product/:id", isAdmin, authMiddleware, updateProduct);
-router.delete("/delete-product/:id", isAdmin, authMiddleware, deleteProduct);
+router.put   ("/update-product/:id", authMiddleware, isAdmin, updateProduct);
+router.delete("/delete-product/:id", authMiddleware, isAdmin, deleteProduct);
 
 module.exports = router;
