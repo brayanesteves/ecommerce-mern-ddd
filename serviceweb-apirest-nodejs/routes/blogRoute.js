b9be9b29efaadfc60d705491e09745a9801d6082
Brayan Esteves <brayan.esteves93@gmail.com>
2023-06-01 15:36:19 -0400

    Get Blog + Collections Postman (Update) [01.06.2023]

--- serviceweb-apirest-nodejs/routes/blogRoute.js
+++ serviceweb-apirest-nodejs/routes/blogRoute.js
@@ -5,6 +5,6 @@ const router  = express.Router();
 
 router.post(       "/add-blog", authMiddleware, isAdmin, createBlog);
 router.put ("/update-blog/:id", authMiddleware, isAdmin, updateBlog);
-router.get (   "/get-blog/:id", authMiddleware, isAdmin, getBlog);
+router.get (   "/get-blog/:id", getBlog);
 
 module.exports = router;
