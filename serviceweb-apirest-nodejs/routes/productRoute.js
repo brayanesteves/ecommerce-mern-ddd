09e8e9f2ff6d90b59b7c66440b85d688f79e6cf6
Brayan Esteves <brayan.esteves93@gmail.com>
2023-06-17 17:02:31 -0400

    [Put] Add to Wish List + Collections Postman (Update) [17.06.2023]

--- serviceweb-apirest-nodejs/routes/productRoute.js
+++ serviceweb-apirest-nodejs/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createProduct, getProduct, getAllProduct, updateProduct, deleteProduct, getAllProductsParams } = require('../controllers/productCtrl');
+const { createProduct, getProduct, getAllProduct, updateProduct, deleteProduct, getAllProductsParams, addToWishlist } = require('../controllers/productCtrl');
 const { isAdmin, authMiddleware } = require('../middlewares/authMiddleware');
 const router  = express.Router();
 
@@ -8,6 +8,7 @@ router.get   (  "/find-product/:id", getProduct);
 router.get   (     "/find-products", getAllProductsParams);
 router.get   (       "/all-product", getAllProduct);
 router.put   ("/update-product/:id", authMiddleware, isAdmin, updateProduct);
+router.put   (          "/wishlist", authMiddleware, isAdmin, addToWishlist);
 router.delete("/delete-product/:id", authMiddleware, isAdmin, deleteProduct);
 
 module.exports = router;
